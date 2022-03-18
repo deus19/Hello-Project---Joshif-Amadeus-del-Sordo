c7020a679baeb7e6ec7a4f347a6d160ebd851139
deus19 <joshifamadeus19@gmail.com>
2022-03-18 12:14:38 +0800

    initial commit

---
+++ testes.py
@@ -0,0 +1,13 @@
+x = int(input())
+
+if x % 3==0 and x % 5==0:
+    print(True)
+else :
+    print(False)
+
+
+def fungsi(z) :
+    if x % 3==0 and x % 5==0:
+        return True
+    else:
+        return False
